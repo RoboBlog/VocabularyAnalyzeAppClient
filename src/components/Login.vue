cb88656d6ddad118b1d3071c575ac67af0512332
roboblog <Robovlogg@gmail.com>
2017-08-19 00:46:17 +0200

    add home&login page

---
+++ src/components/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>Login</h1>
+    <br/>
+    <input type = "text" v-model="credentials.username" placeholder="Enter your username">
+    <br />
+    <br />
+    <input type = "password" v-model="credentials.password" placeholder="Enter your password">
+    <button v-on:click="login">Login</button>//Or Click enter
+    <!--VALIDATE-->
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+    data() {
+      return {
+        credentials: {
+            username: '',
+            password: ''
+        }
+      };
+    },
+    methods: {
+      login: function() {
+          console.log(this.credentials)
+        this.$http.post('http://localhost:8080/login', body).then(response =>{
+          this.words = response.body;
+        }, response =>{
+          alert("Oups");
+        });
+      },
+    }
+  }
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import BootstrapVue from 'bootstrap-vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-
+import Login from '@/components/Login'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(Router)
+Vue.use(VueResource)
 Vue.use(BootstrapVue)
 
 export default new Router({
@@ -15,6 +17,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
